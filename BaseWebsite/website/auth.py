aa0143ae7bcf702799e454acf11a91a6cba62c2d
Jack Campbell <jackwcampbell11@gmail.com>
2022-11-09 11:33:45 -0500

    Updated flash messages.

--- BaseWebsite/website/auth.py
+++ BaseWebsite/website/auth.py
@@ -15,13 +15,13 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Login successfully.', category='success')
+                flash('Login success.', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Incorrect password. Try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Account not found.', category='error')
 
     return render_template("login.jinja", user=current_user)
 
