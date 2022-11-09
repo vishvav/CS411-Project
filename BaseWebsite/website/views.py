d402b1804e39161ee9822988e7187e17c291897e
Jack Campbell <jackwcampbell11@gmail.com>
2022-11-09 14:08:07 -0500

    displays picture of the day on Home page.

--- BaseWebsite/website/views.py
+++ BaseWebsite/website/views.py
@@ -1,9 +1,31 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 
+# import nasapy
+# import os
+from datetime import datetime
+# import urllib.request
+#from gtts import gTTS
+import requests, json
+
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.jinja", user=current_user)
+    # stores APi key and Api URL
+    nasakey = "l1YsgpdqqiBEXpx1IqHfEp1DkqgysDrBvzjIeKVR"
+    url = 'https://api.nasa.gov/planetary/apod?'
+
+    params={
+        'api_key': nasakey,
+        'hd': 'True',
+        'date': datetime.today().strftime('%Y-%m-%d')
+    }
+
+    response = requests.get(url, params=params)
+    # data has copyright, date, explaination, hdurl, media_type, service_version, title, url
+    data = response.json()
+    print(data)
+        
+    return render_template("home.jinja", user=current_user, data=data)
